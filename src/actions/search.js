6dac4b4e5a44873eaf48df5f8f7cb8558082d248
helloitjase <jason.sl.chen@gmail.com>
2019-07-31 17:38:12 -0700

    complete action dispatchers

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,31 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  // videos:
   //TODO:  Write an asynchronous action to handle a video search!
+  return () => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, function(videos) {
+      changeVideo(videos[0]);
+      changeVideoList(videos);
+    })
+  }
 };
 
 export default handleVideoSearch;
+// searchYouTube({key: YOUTUBE_API_KEY, query: 'cats', max: 5}, function(items) {
+  //   console.log(items)});
+
+
+  // return (searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, function(videos) {
+    //       console.log(videos);
+    // (dispatch) => {
+      // }
+      //         dispatch ()
+          // return {
+          //   type: 'SEARCH',
+          //   videos: searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, function(videos) {
+          //           changeVideo(videos[0]);
+          //           changeVideoList(videos);
+          //         }),
+          //   dispatch: (dispatch) => {dispatch(this.videos)}
+          // }
